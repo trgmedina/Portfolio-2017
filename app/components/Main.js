293ba79a6400f4ddff42960099168893245c9f34
Theresa Medina <trgmedina@gmail.com>
2017-08-01 22:20:10 -0500

    adding styles

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,6 +1,6 @@
 // Include React as a dependency
 import React from 'react';
-import {Navbar, NavItem, Icon } from 'react-materialize';
+import {Navbar, NavItem, Icon, Button, Carousel } from 'react-materialize';
 
 // Including the Link component from React Router to navigate within our application without full page reloads
 const Link = require("react-router").Link;
@@ -13,10 +13,80 @@ class Main extends React.Component {
     return (
       <div className="container-fluid">
          
-        <Navbar id= "nav" brand='Theresa R. Medina' right>
-          <NavItem href='get-started.html'>Getting started</NavItem>
-          <NavItem href='components.html'>Components</NavItem>
+        <Navbar id= "nav" brand="Theresa R. Medina" href="#welcome" right>
+          <i className="symbol material-icons">spa</i>
+          <NavItem className="options" href="#about">About</NavItem>
+          <i className="symbol material-icons">assignment</i>
+          <NavItem className="options" href="#projects">Projects</NavItem>
+          <i className="symbol material-icons">mail</i>
+          <NavItem className="options" href="#contact">Contact</NavItem>
         </Navbar>
+
+        <div id="welcome">
+          <div className="blurb">
+            <h1>Full-Stack Web Developer</h1>
+            <p id="summary">Passionate and driven to create outstanding user experiences.</p>
+          </div>
+
+          <div className="more">
+            <h4>Check Out My Work!</h4>
+            <Button floating large className='light' waves='light' icon='expand_more' href="#projects"></Button>
+          </div>
+        </div>
+
+        <div id="about">
+          <h2 className="titles">Experience</h2>
+
+          <div className="icons">
+            <i className="devi devicon-html5-plain-wordmark"></i>
+            <i className="devi devicon-css3-plain-wordmark"></i>
+            <i className="devi devicon-javascript-plain"></i>
+            <i className="devi devicon-jquery-plain-wordmark"></i>
+            <br></br><br></br><br></br>
+            <i className="devi devicon-git-plain"></i>
+            <i className="devi devicon-nodejs-plain"></i>
+            <i className="devi devicon-express-original"></i>
+            <i className="devi devicon-react-original-wordmark"></i>
+            <br></br><br></br><br></br>
+            <i className="devi devicon-mongodb-plain-wordmark"></i>
+            <i className="devi devicon-python-plain-wordmark"></i>
+            <i className="devi devicon-mysql-plain-wordmark"></i>
+            <i className="devi devicon-sequelize-plain-wordmark"></i>
+            <br></br><br></br><br></br>
+            <i className="devi devicon-django-plain"></i>
+            <i className="devi devicon-heroku-original-wordmark"></i>
+            <i className="devi devicon-java-plain-wordmark"></i>
+            <i className="devi devicon-cplusplus-plain"></i>
+          </div>
+        </div>
+
+        <div id="#projects">
+          <h2 className="titles">Projects</h2>
+          <Carousel fixedItem={<Button className="red" waves="light" node='a' href="https://github.com/trgmedina/plantapp" target="_blank">Plantr</Button>}
+          options= {{ fullWidth: true }} images={[
+            'https://github.com/trgmedina/plantapp/blob/master/public/Screenshots/Login-1.PNG?raw=true',
+            'https://github.com/trgmedina/plantapp/raw/master/public/Screenshots/Reminders.PNG?raw=true',
+            'https://github.com/trgmedina/plantapp/raw/master/public/Screenshots/Plants-2.PNG?raw=true'
+          ]} />
+          <br></br>
+          <Carousel fixedItem={<Button className="red" waves="light" node='a' href="https://github.com/mattsells/BarBid" target="_blank">BarBid</Button>}
+          options= {{ fullWidth: true }} images={[
+            'https://github.com/mattsells/barbid/raw/master/public/images/login-screen.png',
+            'https://github.com/mattsells/barbid/raw/master/public/images/patron-listing.png',
+            'https://github.com/mattsells/barbid/raw/master/public/images/patron-accepted.png'
+          ]} />
+          <br></br>
+          <Carousel fixedItem={<Button className="red" waves="light" node='a' href="https://github.com/trgmedina/plantapp" target="_blank">Dinner and a Movie</Button>}
+          options= {{ fullWidth: true }} images={[
+            'https://camo.githubusercontent.com/5ea3de70f6eb45d63e702c42775b456ba416b6eb/687474703a2f2f696d6775722e636f6d2f55356f706c776e2e706e67',
+            'https://camo.githubusercontent.com/9a52a84960a98c08cfe399dea780284b6cbd50e8/687474703a2f2f696d6775722e636f6d2f684465344473762e706e67',
+            'https://camo.githubusercontent.com/64af9bcaab0eb343c1a01e07e4b25d4683f7730e/687474703a2f2f696d6775722e636f6d2f6d735472334c702e706e67'
+          ]} />
+          <br></br>
+        </div>
+
+        <div id="contact">
+        </div>
 
       </div>
     );
